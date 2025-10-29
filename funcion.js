44ed343bdd17f3fe35f2f4a1113afad69b7de96c
sofia59 <sofiazupel4@gmail.com>
2025-10-29 14:46:26 -0300

    modifico inicio css

--- funcion.js
+++ funcion.js
@@ -3,27 +3,26 @@ const API_KEY = "fab76acb4e92bb3931960dd5fda08f1d";
 async function buscarClima() {
   const ciudad = document.getElementById("city").value.trim();
   if (!ciudad) return;
-
+  
   try {
-
     const resp = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(ciudad)}&appid=${API_KEY}&units=metric&lang=es`
     );
+    
     if (!resp.ok) throw new Error("Ciudad no encontrada");
+    
     const datos = await resp.json();
-
     const { main, coord, weather } = datos;
     const temp = main.temp;
     const hum = main.humidity;
     const desc = weather[0].description;
-
   
     const respUV = await fetch(
       `https://api.openweathermap.org/data/2.5/uvi?lat=${coord.lat}&lon=${coord.lon}&appid=${API_KEY}`
     );
     const uvData = await respUV.json();
     const uv = uvData.value;
-
+    
     document.getElementById("resultado").innerHTML = `
       <strong>${ciudad}</strong><br>
       Estado: ${desc}<br>
